f904a9a209d9cd47d2dc76cb537022da504789df
huangzhenjiang <huangzhenjiang@zsoft.com.cn>
2022-01-13 16:47:48 +0800

    添加目录结构

---
+++ src/views/layout/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-container :direction="vertical">
+        <el-header height="">
+            <!-- Header content -->
+        </el-header>
+        <el-container :direction="horizontal">
+            <el-aside width="200px" style="background-color: grey;">
+                <!-- Aside content -->
+            </el-aside>
+            <el-container :direction="vertical">
+                <el-main height="">
+                    <!-- Main content -->
+                    <router-view />
+                </el-main>
+                <el-footer height="">
+                    <!-- Footer content -->
+                </el-footer>
+            </el-container>
+        </el-container>
+    </el-container>
+</template>
